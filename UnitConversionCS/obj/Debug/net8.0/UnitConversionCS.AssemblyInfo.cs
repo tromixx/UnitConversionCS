094c9e362554db44c63102d5715ee16925330989
Tomas Venere <veneret@gmail.com>
2025-01-14 13:59:56 -0500

    Adding nice css ui

--- UnitConversionCS/obj/Debug/net8.0/UnitConversionCS.AssemblyInfo.cs
+++ UnitConversionCS/obj/Debug/net8.0/UnitConversionCS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitConversionCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd4be2810e3b18ec2c31a8fffbafe3ad87e9bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e62020092138e8480e95da8010cb4540b43e5db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitConversionCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitConversionCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
